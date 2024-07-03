0a4b4efa5361d12535a32da244ba2e839394d164
camillapd <camilla.andrade.pereira@gmail.com>
2024-07-03 14:05:17 -0300

    add classe genre

--- lib/model/genre.dart
+++ lib/model/genre.dart
@@ -1,3 +1,27 @@
+import 'dart:convert';
+
 class Genre {
-  
+  int? id;
+  String name;
+
+  Genre({this.id, required this.name});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "id": this.id,
+      "name": this.name,
+    };
+  }
+
+  factory Genre.fromMap(Map<String, dynamic> map) {
+    return Genre(
+      id: map["id"] ??= map["id"],
+      name: map["name"] as String,
+    );
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Genre.fromJson(String source) =>
+      Genre.fromMap(jsonDecode(source) as Map<String, dynamic>);
 }
